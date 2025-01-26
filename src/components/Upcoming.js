56ab498725abd58cde2b391fc6cc973f8debde50
Karrnidh <karrnidh.bs24dmu@spjain.org>
2025-01-26 12:27:26 +0530

    Archiving event card with /

--- src/components/Upcoming.js
+++ src/components/Upcoming.js
@@ -1,15 +1,17 @@
-import React from 'react'
-import Cards from './Cards'
+import React from 'react';
+import Cards from './Cards';
 
 export default function Upcoming() {
   return (
-    <div>
+    <div style={{ height: '100vh', overflowY: 'auto' }}>
       <div className="container text-center">
-  <div className="row row-cols-4">
-        <div className="col"><Cards  image = "yes1.jpg" text= "event" link = "/"/></div>
-        
-  </div>
-</div>
+        <div className="row row-cols-4">
+          {/* <div className="col"><Cards  image = "yes1.jpg" text= "event" link = "/"/></div> */}
+        </div>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
+          <p style={{ color: '#6c757d', fontSize: '1.25rem' }}>Coming Soon!</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
